078e6e501505b96c520ea06d4200078435652d7e
lhlPersonal <724800840@qq.com>
2015-02-26 10:25:42 +0800

    master commit.

--- angular_web/test/AWS/main/config.js
+++ angular_web/test/AWS/main/config.js
@@ -3,8 +3,15 @@
  */
 
 var app = angular.module("awsModule", ["ngRoute"]).config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+<<<<<<< HEAD:angular_web/test/AWS/main/config.js
     $routeProvider
         .when("/userInfo", {templateUrl: "/test/AWS/userInfo.html"})
         .otherwise({redirectTo: "/test/AWS/main/main.html"});
     $locationProvider.html5Mode(true);
+=======
+//    $routeProvider
+//        .when("/userInfo", {templateUrl: "/views/test/AWS/userInfo.html"})
+//        .otherwise({redirectTo: "/views/test/AWS/main.html"});
+//    $locationProvider.html5Mode(true);
+>>>>>>> add userinfo.html.:angular_web/test/AWS/config.js
 }])
