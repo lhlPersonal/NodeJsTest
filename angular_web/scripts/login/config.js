d51895f917b2964ba902b031dfce6ae70a86bf44
lhlPersonal <724800840@qq.com>
2015-03-03 11:00:55 +0800

    new login files.

--- angular_web/scripts/login/config.js
+++ angular_web/scripts/login/config.js
@@ -1,3 +1,4 @@
 /**
  * Created by bulusli on 2015/2/27.
  */
+var app=angular.module("dvLoginApp",["ngMessages"]);
--- angular_web/scripts/login/controller.js
+++ angular_web/scripts/login/controller.js
@@ -1,3 +1,6 @@
 /**
  * Created by bulusli on 2015/2/27.
  */
+app.controller("dvLoginController",["dvLoginService",function(dvLoginService){
+
+}]);
