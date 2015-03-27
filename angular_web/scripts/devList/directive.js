0fc7b21fa170a2e911f5713983e05e4895fade84
lhlPersonal <724800840@qq.com>
2015-03-27 14:36:19 +0800

    beta 1.0.0.0 commit.

--- angular_web/scripts/devList/directive.js
+++ angular_web/scripts/devList/directive.js
@@ -9,3 +9,14 @@ app.directive("dvGrprepeat", function () {
         templateUrl: "/views/devList/templ/left_name.html"
     }
 });
+
+app.directive("dvShowbar", function () {
+    return {
+        restrict: "A",
+        link: function (scope, element, attr) {
+            element.on("click", function () {
+                element.next("ul").css({top: element.offset().top - 40, left: element.offset().left + 15});
+            })
+        }
+    }
+});
